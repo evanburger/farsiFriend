767aeae98c034c997b83825fbc26e06cff655f62
Evan Mildenberger <emildenberger>
2018-08-15 10:58:44 -0700

    0.4.0 - added feature to make entries via a text file. refactored the code to work with new feature

--- updateVocab.py
+++ updateVocab.py
@@ -30,6 +30,9 @@ import pymysql as sql
 DB = 'PersianVocabulary'
 USER = 'root'
 
+fileMode = False
+
+
 #  The fileHandler writes info and above to a file while the streamHandler writes to the console.
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -45,20 +48,15 @@ logger.addHandler(fileHandler)
 streamHandler = logging.StreamHandler()
 streamHandler.setFormatter(formatter)
 
-#  The system arguments set the level of the streamHandler or displays optional sys arugments.
+#  These sys arguments set the logging level.
 try:
-    if (sys.argv[1] == "--help" or sys.argv[1] == "-h"):
-        print("""python updateVocab.py [-h | -d | -f]
-    Optional arguments:
-    --help, -h      Display optional arguments
-    --debug, -d     Run program with all logging displayed
-    --full, -f      Run program with only errors displayed""")
-        quit()
-
-    elif (sys.argv[1] == "--debug" or sys.argv[1] == "-d"):
+    if (sys.argv[1] == "--debug" or sys.argv[1] == "-d"):
         streamHandler.setLevel(logging.DEBUG)
     elif (sys.argv[1] == "--full" or sys.argv[1] == "-f"):
         streamHandler.setLevel(logging.CRITICAL)
+    elif (sys.argv[1] == "--bulk" or sys.argv[1] == "-b"):
+        #  This is to avoid having the logger show all debugging info when in bulk entry mode.
+        streamHandler.setLevel(logging.INFO)    
 except IndexError: #  This exception occurs when no sys arguments are given.
     streamHandler.setLevel(logging.INFO)
 
@@ -69,22 +67,9 @@ class InputTypeError(TypeError):
     """This error is to be raised when input made by the user doesn't meet the requirements of the program."""
 
 
-def getInput():
-
-    logger.debug("getInput started")
-    userInput = input("Enter English, Finglish, Persian, and type seperated by a space: ").lower()
-
-    if userInput == 'q':
-        print("Update Vocab exited")
-        quit()
-    elif userInput == "":
-        userList, repeat = None, False
-        logger.info(f"repeat = {repeat}")
-        return (userList, repeat)
-    userList = userInput.split(" ")
-    logger.info(f"userList = {userList}")
-
+def consolidateWords(userList, fileMode):
     #  Check to see if there are compound verbs.
+    logger.debug("consolidateWords started")
     try:
         if len(userList) == 4: #  This is the usual case, so no action is required.
             logger.info("userList length = 4")
@@ -102,7 +87,57 @@ def getInput():
     except InputTypeError as error:
         logger.error(error)
         logger.error(f"InputTypeError: only {len(userList)} words entered, 4, 6 or 7 expected")
-        getInput()
+        if fileMode:
+            quit()
+        else:
+            getInput()
+    logger.debug("consolidateWords ended")
+    return userList
+
+def updateVocabFromFile(filePath):
+    print("""
+Update Vocab --File Mode--
+This program is an interface to add entries into the PersianVocabulary database.
+""")
+    with open(filePath) as file:
+        data = []
+        fileMode = True
+        for line in file:
+            userList = line.strip("\n").lower().split(" ")
+            userList = consolidateWords(userList, fileMode)
+            data = storeInput(data, userList)
+            logger.info(f"data = {data}")
+        updateVocab(data, DB, USER, fileMode)
+
+def storeInputFromFile():
+    filePath = input("Enter the file path of the text file to enter: ")
+    with open(filePath) as file:
+        data = []
+        for line in file:
+            userList = line.strip("\n").lower().split(" ")
+            logger.info(f"userList = {userList}")
+            data = storeInput(data, userList)
+        logger.info(f"data = {data}")
+    return data
+
+def getInput():
+
+    logger.debug("getInput started")
+    userInput = input("Enter English, Finglish, Persian, and type seperated by a space: ").lower()
+
+    if userInput == 'q':
+        print("Update Vocab exited")
+        quit()
+    elif userInput == "":
+        userList, repeat = None, False
+        logger.info(f"repeat = {repeat}")
+        return (userList, repeat)
+    userList = userInput.split(" ")
+    logger.info(f"userList = {userList}")
+
+    fileMode = False
+    userList = consolidateWords(userList, fileMode)
+    logger.info(f"userList = {userLIst}")
 
     logger.debug("getInput ended")
     repeat = True
@@ -121,7 +156,7 @@ def getPassword():
     logger.debug("getPassword ended")
     return password
 
-def updateVocab(data, DB, USER):
+def updateVocab(data, DB, USER, fileMode):
 
     logger.debug("updateVocab started")
     password = getPassword()
@@ -135,41 +170,71 @@ def updateVocab(data, DB, USER):
         queries.append("INSERT INTO words VALUES (%s, %s, %s, %s, NULL);")
         logger.info(f"queries = {queries}")
     for query, datum in zip(queries, data):
-        logger.info(f"query = {query}")
-        logger.info(f"datum = {datum}")
         c.execute(query, datum) #  Inputs are sanitized
         logger.debug("query executed")
 
     conn.commit()
     conn.close()
     logger.debug("updateVocab ended")
+    if fileMode:
+        print("Update successful.")
+        quit()
 
 
-def main():
-    print("""
+def main(fileMode):
+
+    logger.debug("main started")
+
+    if fileMode: #  This occurs when --bulk or -b are given as a sys argument.
+        logger.info(f"fileMode = {fileMode}")
+        print("""
+Update Vocab --File Mode--
+This program is an interface to add entries into the PersianVocabulary database.
+""")
+        data = storeInputFromFile()
+        updateVocab(data, DB, USER, fileMode)
+
+    elif not fileMode:
+        logger.info(f"fileMode = {fileMode}")
+        print("""
 Update Vocab
 This program is an interface to add entries into the PersianVocabulary database.
 
 Enter words as directed. Press enter with nothing entred to save the words. Enter q to exit.
 """)
-    logger.debug("main started")
-    userList, repeat = getInput()
-    
-    data = []
-
-    while repeat:
-        data = storeInput(data, userList)
-        logger.info(f"data = {data}")
         userList, repeat = getInput()
+    
+        data = []
 
-    updateVocab(data, DB, USER)
+        while repeat:
+            data = storeInput(data, userList)
+            logger.info(f"data = {data}")
+            userList, repeat = getInput()
 
-    print("""Update successful
-Entries made:""")
-    for line in data:
-        print(line[0], line[1], line[2], line[3])
+    updateVocab(data, DB, USER, fileMode)
+
+    print("Update successful.")
+
+
+#  These optional sys arguments show either available options or allow the input of file contents.
+try:
+    if (sys.argv[1] == "--help" or sys.argv[1] == "-h"):
+        print("""python updateVocab.py [-h | -d | -f]
+    Optional arguments:
+    --help, -h      Display optional arguments
+    --debug, -d     Run program with all logging displayed
+    --full, -f      Run program with only errors displayed""")
+        quit()
+
+    elif (sys.argv[1] == "--bulk" or sys.argv[1] == "-b"):
+        try:
+            updateVocabFromFile(sys.argv[2])
+        except IndexError: #  This occurs when no file name is provided as a sys argument.
+            fileMode = True #  The control flow continues to main
+except IndexError: #  This exception occurs when no sys arguments are given.
+    pass
 
 
 if __name__ == "__main__":
-    main()
+    main(fileMode)
 
